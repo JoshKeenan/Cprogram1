099c3787c949405bace3b1ea5d9dd6e142ef53a7
User <user@halls-10-40-170-80.wireless.leeds.ac.uk>
2017-10-19 22:21:13 +0100

    Replaced control if with While loop

--- main.c
+++ main.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
 
     // Initialisation of variables by type
     int start;
 
-    // Start block, output first menu and get user input
-    printf("Hello, What would you like to do?\n");
-    printf("1. Compute and Print the Bill for a Customer\n");
-    printf("2. Show Sums and Statistics\n");
-    printf("3. Quit the program\n\n");
-    printf("Please enter your choice:");
-    scanf("%i", &start);
-
     // Test user input and provide response
-
-    if(start > 3 || start < 1){
-        printf("That wasn't a valid response, do you want to try again?");
-
+    while(start > 3 || start < 1) {
+        // Output first menu and get user input
+        printf("Hello, What would you like to do?\n");
+        printf("1. Compute and Print the Bill for a Customer\n");
+        printf("2. Show Sums and Statistics\n");
+        printf("3. Quit the program\n\n");
+        printf("Please enter your choice:");
+        scanf("%i", &start);
     }
+
 
 
     return 0;
