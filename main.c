85f7ca9472c2071d16f893fd3babde4070d3f14d
User <user@halls-10-40-175-87.wireless.leeds.ac.uk>
2017-10-23 16:31:34 +0100

    Added first stage of first calculation

--- main.c
+++ main.c
@@ -6,9 +6,10 @@ int main() {
 
     // Initialisation of variables by type
     int start;
+    float billCalc, total;
 
     // Test user input and provide response
-    while(start > 3 || start < 1) {
+    while (start > 3 || start < 1) {
         // Output first menu and get user input
         printf("Hello, What would you like to do?\n");
         printf("1. Compute and Print the Bill for a Customer\n");
@@ -18,8 +19,24 @@ int main() {
         scanf("%i", &start);
     }
 
-    if(start == 1)
-        printf("1 was your choice");
+    if (start == 1){
+        printf("Please enter your bill:");
+        scanf("%f", &billCalc);
+
+        if (billCalc <= 5.49){
+            total = billCalc * 0.20;
+        }
+        else if (billCalc > 5.49 && billCalc <= 12.49){
+            total = ((billCalc - 5.49) * 0.35) + (5.49 * 0.2);
+        }
+        else if (billCalc > 12.49 && billCalc <= 25.49){
+            total = ((billCalc - 12.49) * 0.5) + ((billCalc - 5.49) * 0.35) + (5.49 * 0.2);
+        }
+
+        printf("%f", total);
+
+    }
+
     else if(start == 2)
         printf("2 was your choice");
     else if(start == 3) {
