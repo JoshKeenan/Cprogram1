e9cb84ad13266fe70bbde5dba3017aceffa146fc
User <user@campus-10-41-28-138.wireless.leeds.ac.uk>
2017-10-24 14:19:13 +0100

    First stage of calculation complete, fixed formatting issues

--- main.c
+++ main.c
@@ -5,48 +5,54 @@
 int main() {
 
     // Initialisation of variables by type
-    int start;
-    float billCalc, total;
+    int billCalc, start, quit;
+    float total;
 
-    // Test user input and provide response
-    while (start > 3 || start < 1) {
-        // Output first menu and get user input
-        printf("Hello, What would you like to do?\n");
-        printf("1. Compute and Print the Bill for a Customer\n");
-        printf("2. Show Sums and Statistics\n");
-        printf("3. Quit the program\n\n");
-        printf("Please enter your choice:");
-        scanf("%i", &start);
-    }
+    while (quit != 1) {
+        // Test user input and provide response
+        while (start != 0) {
+            // Output first menu and get user input
+            printf("Hello, What would you like to do?\n");
+            printf("1. Compute and Print the Bill for a Customer\n");
+            printf("2. Show Sums and Statistics\n");
+            printf("3. Quit the program\n\n");
+            printf("Please enter your choice:");
+            scanf("%i", &start);
 
-    if (start == 1){
-        printf("Please enter your water consumption:");
-        scanf("%f", &billCalc);
 
-        if (billCalc <= 5.49){
-            total = billCalc * 0.20;
-            total = total + ((0.95 * total) * 0.25) + 10 + (91 * 0.10);
+            if (start == 1) {
+                printf("Please enter your water consumption:");
+                scanf("%i", &billCalc);
+
+                if (billCalc <= 5)
+                    total = (billCalc * 0.20);
+
+                else if (billCalc > 5 && billCalc <= 12)
+                    total = ((billCalc - 5.00) * 0.35) + 1;
+
+                else if (billCalc > 12 && billCalc <= 25)
+                    total = ((billCalc - 12.00) * 0.50) + 3.45;
+
+                else if (billCalc > 25 && billCalc <= 40)
+                    total = ((billCalc - 25) * 0.75) + 9.95;
+
+                else
+                    total = ((billCalc - 40) * 2.5) + 21.2;
+
+                total = total + ((0.95 * billCalc) * 0.25) + 10 + (91 * 0.10);
+                printf("%f \n\n", total);
+
+                printf("---------------------------------\n\n");
+
+            } else if (start == 2)
+                printf("2 was your choice\n");
+
+            else if (start == 3) {
+                printf("End of program..\n");
+                start = 0;
+                quit = 1;
+            }
         }
-        else if (billCalc > 5.49 && billCalc <= 12.49){
-            total = ((billCalc - 5.49) * 0.35) + (5.49 * 0.20);
-            total = total + ((0.95 * total) * 0.25) + 10 + (91 * 0.10);
-        }
-        else if (billCalc > 12.49 && billCalc <= 25.49){
-            total = ((billCalc - 12.49) * 0.50) + ((billCalc - 5.49) * 0.35) + (5.49 * 0.20);
-            total = total + ((0.95 * total) * 0.25) + 10 + (91 * 0.10);
-        }
-
-        printf("%f", total);
-
     }
-
-    else if(start == 2)
-        printf("2 was your choice");
-    else if(start == 3) {
-        printf("End of program..");
-        exit(0);
-    }
-
-
     return 0;
 }
