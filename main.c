b87b2774a87b7b2a27df51c7bdf04e9fb53811bf
User <user@halls-10-40-41-182.wireless.leeds.ac.uk>
2017-10-25 02:23:33 +0100

    Bill calculator completed.

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 int main() {
 
     // Initialisation of variables by type
-    int billCalc, start, quit, resCom;
-    float total;
+    int billCalc, start, quit, resCom, standing;
+    float total, waterCharge, waste, surface;
 
     while (quit != 1) {
         // Test user input and provide response
@@ -29,36 +29,45 @@ int main() {
                 }
                 if (resCom == 1) {
                     if (billCalc <= 5)
-                        total = (billCalc * 0.20);
+                        waterCharge = (billCalc * 0.20);
 
                     else if (billCalc > 5 && billCalc <= 12)
-                        total = ((billCalc - 5.00) * 0.35) + 1;
+                        waterCharge = ((billCalc - 5.00) * 0.35) + 1;
 
                     else if (billCalc > 12 && billCalc <= 25)
-                        total = ((billCalc - 12.00) * 0.50) + 3.45;
+                        waterCharge = ((billCalc - 12.00) * 0.50) + 3.45;
 
                     else if (billCalc > 25 && billCalc <= 40)
-                        total = ((billCalc - 25) * 0.75) + 9.95;
+                        waterCharge = ((billCalc - 25) * 0.75) + 9.95;
 
                     else
-                        total = ((billCalc - 40) * 2.5) + 21.2;
+                        waterCharge = ((billCalc - 40) * 2.5) + 21.2;
 
-                    total = total + ((0.95 * billCalc) * 0.25) + 10 + (91 * 0.10);
+                    standing = 10;
+                    surface = (91 * 0.1);
+                    waste = (0.95 * billCalc) * 0.25;
+                    total = waterCharge + waste + standing + surface;
+                    printf("\nCustomer type: Residential\n");
+                    printf("VAT: %i\n", 0);
                 }
+
                 else if (resCom == 2){
-                    total = (billCalc * 2.5) + ((0.95 * billCalc) * 2.0) + 50.0 + (1.30 * 91.0);
+                    standing = 50;
+                    surface = 1.30 * 91.0;
+                    waste = (0.95 * billCalc) * 2;
+                    waterCharge = (billCalc * 2.5);
+                    total = waterCharge + waste + standing + surface;
                     total = total + (total * 0.2);
-
+                    printf("\nCustomer type: Commercial\n");
+                    printf("VAT: %f\n", (total * 0.2));
                 }
 
-                if (resCom == 1)
-                    printf("Customer type: Residential");
-
-                else
-                    printf("Customer type: Commercial");
-
-                printf("Total cost: %f", total);
-                total = 0;
+                printf("Water consumption: %i\n", billCalc);
+                printf("Waste water charge: %f\n", 0.95 * waterCharge);
+                printf("Surface water charge: %i\n", 10);
+                printf("Standing charge: %i\n", standing);
+                printf("Water charge: %f\n", waterCharge);
+                printf("Total cost: %f\n", total);
                 resCom = 0;
                 printf("\n\n---------------------------------\n\n");
 
